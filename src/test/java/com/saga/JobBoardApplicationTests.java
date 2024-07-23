4482c169a57879ddf566d8a6e6806e9647eb0cc8
sushant008 <129641196+sushant008@users.noreply.github.com>
2024-07-23 18:08:52 +0530

    First

---
+++ src/test/java/com/saga/JobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
